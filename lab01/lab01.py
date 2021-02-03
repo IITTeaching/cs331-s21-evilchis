c09a1294f7b7243d206c19c6300cd2c49747e29c
Tomachango <Tomachango4071@gmail.com>
2021-02-03 16:18:41 -0600

    completed lab01

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -22,7 +22,12 @@ def captured_output():
 
 # implement this function
 def is_perfect(n):
-    pass
+    sum = 0
+    for x in range(1, n):
+        if n % x == 0:
+            sum += x
+    return sum == n
+    # hi
 
 # (3 points)
 def test1():
@@ -33,6 +38,10 @@ def test1():
         tc.assertFalse(is_perfect(n), '{} should not be perfect'.format(n))
     for n in range(30, 450):
         tc.assertFalse(is_perfect(n), '{} should not be perfect'.format(n))
+    # print('completed1')
+
+def result1(num):
+    print(is_perfect(num))
 
 #################################################################################
 # EXERCISE 2
@@ -40,7 +49,14 @@ def test1():
 
 # implement this function
 def multiples_of_3_and_5(n):
-    pass
+    sum = 0
+    for x in range(1, n):
+        if x % 3 == 0:
+            sum += x
+        elif x % 5 == 0:
+            sum += x
+    return sum
+    # hi
 
 # (3 points)
 def test2():
@@ -48,18 +64,26 @@ def test2():
     tc.assertEqual(multiples_of_3_and_5(10), 23)
     tc.assertEqual(multiples_of_3_and_5(500), 57918)
     tc.assertEqual(multiples_of_3_and_5(1000), 233168)
+    # print('completed2')
 
 #################################################################################
 # EXERCISE 3
 #################################################################################
 def integer_right_triangles(p):
-    pass
+    dimensions = [(a,b,p-a-b)
+                    for a in range (1,p//3) #(1,p//3)
+                    for b in range (a+1,p//2)
+                    if (a**2 + b**2 == (p-(a+b))**2) and (a+b+(p-(a+b))==p)]
+    # return dimensions
+    return len(dimensions)
+    # hi
 
 def test3():
     tc = unittest.TestCase()
     tc.assertEqual(integer_right_triangles(60), 2)
     tc.assertEqual(integer_right_triangles(100), 0)
     tc.assertEqual(integer_right_triangles(180), 3)
+    # print('completed3')
 
 #################################################################################
 # EXERCISE 4
@@ -67,7 +91,33 @@ def test3():
 
 # implement this function
 def gen_pattern(chars):
-    pass
+    length = len(chars)
+    dots = (length*2 - 1)*2 - 1
+    string = ''
+    string_list = []
+    for i in range(length):
+        string1 = ''
+        string += chars[i]
+        length1 = len(string)
+        for j in range(0, length1):
+            if j % 2 != 0:
+                string1 += chars[length -1 - j].center(3, '.')
+            else:
+                string1 += chars[length - 1 - j]
+        for k in range(i - 1, -1, -1):
+            if k % 2 != 0:
+                string1 += chars[length - 1 - k].center(3, '.')
+            else:
+                string1 += chars[length - 1 - k]
+        string1 = string1.center(dots, '.')
+        string_list.append(string1)
+
+    print('\n'.join(string_list))
+    bottom_of_diamond_string_list = list(reversed(string_list))[1:]
+    print('\n'.join(bottom_of_diamond_string_list))
+    # hi
+
+
 
 def test4():
     tc = unittest.TestCase()
@@ -143,6 +193,7 @@ p.o.n.m.l.k.j.i.h.g.f.e.d.c.b.a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p
 ..............................p..............................
 """.strip()
 )
+    # print('completed4')
 
 #################################################################################
 # RUN ALL TESTS
